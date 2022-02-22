57f75acfb7589e15ae4bc7df88baa21c44ca9abb
jhenryshaw4 <jhenryshaw4@gmail.com>
2022-02-22 10:37:35 -0500

    10.35

--- 01_read_json/read_json.py
+++ 01_read_json/read_json.py
@@ -1,23 +1,28 @@
 # ---------------------------------------------
 # import package to read json
 # ---------------------------------------------
-
+import json
 
 # ---------------------------------------------
 #  write function to load file and parse json
 # ---------------------------------------------
 
 def readJson(file):
-    pass
+    with open(file) as p:
+        return json.load(p)
 
 # ---------------------------------------------
 #  call 'readJson', load salaries
 # ---------------------------------------------
 
-
+salaries = readJson('./data.json')
+print(salaries)
 # ---------------------------------------------
 #  print all salaries
 # ---------------------------------------------
+
+for salary in salaries['data']:
+    print(float(salary[18]))
 
 
 # ---------------------------------------------
