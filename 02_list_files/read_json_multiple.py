05726b8f7bfffae5a514931874fb1b390a751d8e
jhenryshaw4 <jhenryshaw4@gmail.com>
2022-02-22 10:37:52 -0500

    -

--- 02_list_files/read_json_multiple.py
+++ 02_list_files/read_json_multiple.py
@@ -2,13 +2,29 @@
 # import package to read json
 # import package to walk file system
 # -------------------------------------
-
+import json
+import glob
 
 # -----------------------------------
 #  list all files
 # -----------------------------------
+pattern = "./data/*/*.json"
+#* gives everything
+#.json just gives json files
+data =[]
+for file in glob.glob(pattern):
+    data.append(file)
 
+print(data)
 
 # -----------------------------------
 #  loop through files, parse json
 # -----------------------------------
+def readJson(file):
+    with open(file) as p:
+        return json.load(p)
+
+
+for file in data:
+    file = readJson(file)
+    print(file)
--- 03_validate_json/validate.py
+++ 03_validate_json/validate.py
@@ -9,7 +9,29 @@ def removeQuotes(string):
     pass
 
 def validate(salary):
-    pass
+    try:
+        key = ['id']
+        key = ["position"]
+        key = ["created_at"]
+        key = ["created_meta"]
+        key = ["updated_at"]
+        key = ["updated_meta"]
+        key = ["meta"]
+        key = ["name"]
+        key = ["title"]
+        key = ["department_name"]
+        key = ["regular"]
+        key = ["retro"]
+        key = ["other"]
+        key = ["overtime"]
+        key = ["injured"]
+        key = ["detail"]
+        key = ["quinn"]
+        key = ["total_earnings"]
+        key = ["zip"]
+    except Exception as e:
+        return False
+    return
 
 # -----------------------------------
 #  parse json function
@@ -17,8 +39,24 @@ def validate(salary):
 
 # load file and parse json
 def readJson(file):
-    pass
+    with open(file) as p:
+        return json.load(p)
+
 
 # -----------------------------------
 #  loop files, parse, validate
 # -----------------------------------
+
+pattern = "./data/*/*.json"
+#* gives everything
+#.json just gives json files
+data =[]
+for file in glob.glob(pattern):
+    data.append(file)
+
+for file in data:
+    salaries = readJson(file)
+
+    #validate here
+    for salary in salaries:
+        print("file:", file, "-validation:", validate(salary))
