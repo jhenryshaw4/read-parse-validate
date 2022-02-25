61b1e91252173423c02a640b74848926128e1818
jhenryshaw4 <jhenryshaw4@gmail.com>
2022-02-25 12:01:24 -0500

    class2.25

--- 02_list_files_mbta/read_json_multiple.py
+++ 02_list_files_mbta/read_json_multiple.py
@@ -1,8 +1,26 @@
 import json
 import glob
+import mysql.connector
 
 # -----------------------------------
 #  use glob
 # -----------------------------------
 
+#need pattern/path
+pattern = "./data/*/*.json"
+files = []
 # list all files in data directory
+for file in glob.glob(pattern):
+    files.append(file)
+
+#parse files
+#make functn??
+def readJson(file):
+    with open(file) as parsed:
+        parsed = json.load(file)
+    return parsed
+    
+
+for file in files:
+    inspect = readJson(file)
+    print(inspect)
